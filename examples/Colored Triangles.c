d28988ad4fdc44ff6540aa3c6463d83a01d40a00
lamelimit <217394269+lamelimit@users.noreply.github.com>
2025-07-05 03:51:25 -0700

    Create Colored Triangles.c

---
+++ examples/Colored Triangles.c
@@ -0,0 +1,53 @@
+#include "MauLib.h"
+#include <stdio.h>
+
+
+
+int main() {
+	struct MauContext* ctx = mauCreateContext(800, 450, "window");
+	if (ctx == NULL) {
+		printf("Ctx is NULL");
+		mauDestroyContext(ctx);
+		return -1;
+	}
+	MauVec3 triPos = mauCreateVec3FromValues(0.0f, 0.0f, 0.0f);
+	MauVec4 triColor = mauCreateVec4FromValues(1.0f, 0.0f, 0.0f, 1.0f);
+	MauVec2 triDim = mauCreateVec2FromValues(0.1f, 0.1f);
+	MauTriangleColor t = mauCreateTriangleColor(&triPos, &triColor, &triDim);
+	
+	MauVec3 tP = mauCreateVec3FromValues(0.5f, 0.5f, 0.0f);
+	MauVec4 tC = mauCreateVec4FromValues(0.0f, 1.0f, 0.0f, 1.0f);
+	MauVec2 tD = mauCreateVec2FromValues(0.1f, 0.1f);
+	MauTriangleColor tr = mauCreateTriangleColor(&tP, &tC, &tD);
+	
+
+	MauVec3 movingTriPos = mauCreateVec3FromValues(0.0f, 0.0f, 0.0f);
+	MauVec4 movingTriColor = mauCreateVec4FromValues(0.4f, 0.2f, 0.0f, 1.0f);
+	MauVec2 movingTriDim = mauCreateVec2FromValues(.1f, .1f);
+	
+
+	while (!mauWindowShouldClose(ctx)) {
+		if (mauGetKey(MAU_KEY_RIGHT, ctx)) {
+			mauVec3SetX(&movingTriPos, movingTriPos.x + 0.01f);
+			
+		}
+		else if (mauGetKey(MAU_KEY_LEFT, ctx)) {
+			mauVec3SetX(&movingTriPos, movingTriPos.x - 0.01f);
+		}
+		else if (mauGetKey(MAU_KEY_UP, ctx)) {
+			mauVec3SetY(&movingTriPos, movingTriPos.y + .01f);
+		}
+		else if (mauGetKey(MAU_KEY_DOWN, ctx)) {
+			mauVec3SetY(&movingTriPos, movingTriPos.y - .01f);
+		}
+		MauTriangleColor movingTri = mauCreateTriangleColor(&movingTriPos, &movingTriColor, &movingTriDim);
+		mauBeginDrawing(ctx);
+		mauClearColor(MAU_BLACK);
+		mauDrawTriangleColor(&t, ctx);
+		mauDrawTriangleColor(&tr, ctx);
+		mauDrawTriangleColor(&movingTri, ctx);
+		mauEndDrawing(ctx);
+	}
+	mauDestroyContext(ctx);
+}
+
